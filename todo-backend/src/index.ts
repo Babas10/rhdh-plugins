a0c16292f93532643004c12870c3ea378a5b8ea0
Babas10 <32065458+Babas10@users.noreply.github.com>
2026-08-13 09:28:36 +0200

    Update index.ts

--- todo-backend/src/index.ts
+++ todo-backend/src/index.ts
@@ -1,2 +1,2 @@
-// Re-export the community todo-backend plugin as a dynamic plugin for RHDH.
+//  Re-export the community todo-backend plugin as a dynamic plugin for RHDH.
 export * from '@backstage-community/plugin-todo-backend';
